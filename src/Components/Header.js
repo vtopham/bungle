3f1cca2ad0a06240b68cc7c764ffcbef470f529e
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-03-30 11:44:23 -0700

    init

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,14 @@
 //The header should appear at the top of every page
 //Only Anthony changes this code
 
-import React from "react"
+import React from "react";
 
-function Header (props) {
-
-    return(
-        <>
-        <p>This is the header</p>
-        </>
-    )
+function Header(props) {
+  return (
+    <>
+      <p>This is the header</p>
+    </>
+  );
 }
 
-export default Header
+export default Header;
