7982fd74fe7a94383c8fa9fe9ca1b26b2fd8b31f
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-03-30 16:23:28 -0700

    finished with the bolier plated for the about page, added links and profile imgs

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,15 +1,39 @@
 //The about page is linked to from the footer
 //Only Anthony changes this code
 
-import React from "react"
+import React from "react";
+import Profile from "./Profile";
 
-function About (props) {
+function About(props) {
+  return (
+    <div className="about-container">
+      <div className="profile-container">
+        <Profile
+          image="https://ca.slack-edge.com/T4JUEB3ME-UQPHHCJ5U-516d22802d4b-512"
+          name="anthony amaro"
+          github="https://github.com/anthonyamaro15"
+        />
+        <Profile
+          image="https://ca.slack-edge.com/T4JUEB3ME-USWMDPP3R-467c9f1ca9a3-512"
+          name="victoria topham"
+          github="https://github.com/vtopham"
+        />
+      </div>
+      <div className="zomato">
+        <h1>with special thanks to</h1>
 
-    return(
-        <>
-        <p>This is the about page</p>
-        </>
-    )
+        <div className="zomato-logo">
+          <a
+            href="https://www.zomato.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            zomato
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
