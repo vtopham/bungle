e3c653525a5414ff19f4a8e6ff4125c02dbe8ba7
Victoria  Topham <topham.victoria@gmail.com>
2020-03-31 10:58:35 -0700

    restaurant components and basic styling are here

--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -11,17 +11,16 @@ function Review (props) {
 
     for (let i = 0; i < numOfStars; i++) {
         stars.push(1);
-        console.log(stars);
     }
    
     return(
-        <>
+        <div className = "full-review">
         <p className = "quote">{reviewText}</p>
-        <p className = "reviewer">-{reviewer}</p>
-        <span>
+        <span className = "rating">
             {stars.map( _ => <GoStar/>)}
+            <span className = "reviewer">-{reviewer}</span>
         </span>
-        </>
+        </div>
     )
 }
 function Restaurant (props) {
@@ -30,9 +29,8 @@ function Restaurant (props) {
         <>
         <div className = "restaurant-container">
             <h1>Restaurant Name</h1>
-            <div className="rating">
-                <span>Restaurant Rating</span>
-                <span>
+            <div>
+                <span className = "rating">
                     <GoStar />
                     <GoStar />
                 </span>
@@ -45,13 +43,13 @@ function Restaurant (props) {
                 <button>Google Maps</button>
             </div>
             <div className = "restaurant-reviews">
-                <Review numOfStars = {2} reviewer = {`Victoria`} reviewText = {`Worst place I've ever eaten`}/>
-                <Review numOfStars = {2} reviewer = {`Victoria`} reviewText = {`Worst place I've ever eaten`}/>
-                <Review numOfStars = {2} reviewer = {`Victoria`} reviewText = {`Worst place I've ever eaten`}/>
+                <Review numOfStars = {2} reviewer = {`Victoria`} reviewText = {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque nibh et nunc finibus pellentesque. Sed tempus lectus vitae porttitor commodo. Nam pulvinar ante in volutpat eleifend. Mauris vitae ligula dolor. Nullam feugiat cursus ligula, nec consequat massa tempor vitae.`}/>
+                <Review numOfStars = {3} reviewer = {`Victoria`} reviewText = {`Aenean id accumsan ante, nec tempus lorem. Maecenas vitae nibh at metus euismod varius sagittis non orci. Quisque facilisis nec lorem in vulputate. Sed viverra orci sed velit mollis vestibulum. In mollis mattis nunc, non convallis eros venenatis quis. Quisque auctor erat nec tincidunt semper. Etiam sed sagittis sem, id convallis quam. Nam feugiat, augue id sagittis accumsan, est justo lobortis augue, sit amet pulvinar purus tellus quis turpis. `}/>
+                <Review numOfStars = {2} reviewer = {`Victoria`} reviewText = {`Donec sed dictum justo. Vivamus feugiat faucibus ipsum non fermentum. Cras eu efficitur enim, a elementum nisi. Sed interdum tellus sed ex pretium dictum. Integer metus mauris, viverra vel lectus in, hendrerit aliquam arcu. Pellentesque sodales nulla eu urna vestibulum, ac mattis ligula tempus.`}/>
             </div>
             <div className = "go-back-home">
                 <Link to = "/#search">
-                    <button>Try Again</button>
+                    <button>Back To Search</button>
                 </Link>
             </div>
 
