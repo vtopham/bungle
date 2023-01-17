ded1778b0ac208fde8b5344bd1aca2cc5d5bf888
Victoria  Topham <topham.victoria@gmail.com>
2023-01-16 17:00:48 -0800

    Set up sass

--- bungle/src/App.tsx
+++ bungle/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import './App.css';
+import './App.scss';
 
 import Home from './pages/home';
 import Search from './pages/search';
---
+++ bungle/src/components/Header.tsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+    return(
+        <div className = "header-container">
+            <p>This is the header</p>
+        </div>
+    )
+}
+
